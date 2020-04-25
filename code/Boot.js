a2625c015d33543fe0e71945f6b5711063dfce5a
Luis Antonio <luis.antonio.lima.silva@gmail.com>
2020-04-25 04:47:27 +0100

    adds start menu

--- code/Boot.js
+++ code/Boot.js
@@ -7,7 +7,15 @@ var bootState = {
       ["boundsHorizontal", "assets/boundsHorizontal.png"],
       ["backgroundLevel", "assets/level-background.png"],
       ["bigMack", "assets/Gordo.png"],
-      ["torchInverted", "assets/torchInverted.png"]
+      ["torchInverted", "assets/torchInverted.png"],
+      ["menuBackground", "assets/menuBackground.png"],
+      ["startBtn", "assets/startBtn.png"],
+      ["optionsBtn", "assets/optionsBtn.png"],
+      ["helpBtn", "assets/helpBtn.png"],
+      ["rankingBtn", "assets/rankingBtn.png"],
+      ["titleInline", "assets/title_inline.png"],
+      ["lilPeanutImg", "assets/lil.png"],
+      ["bigMackImg", "assets/Gordo.png"],
     ]);
     game.loadSpritesheet([["lilPeanut", "assets/lilPeanutSprite.png"]]);
   },
--- code/Menu.js
+++ code/Menu.js
@@ -1,6 +1,27 @@
 var menuState = {
-  preload: () => {},
+  preload: () => {
+  },
   create: () => {
-    game.phaser.state.start("Level1");
+    var background = game.phaser.add.sprite(0, 0, "menuBackground");
+    var startBtn = game.phaser.add.sprite(330, 150, "startBtn");
+    var optionsBtn = game.phaser.add.sprite(330, 220, "optionsBtn");
+    var helpBtn = game.phaser.add.sprite(330, 290, "helpBtn");
+    var rankingBtn = game.phaser.add.sprite(330, 360, "rankingBtn");
+    var titleInline = game.phaser.add.sprite(100, -75, "titleInline");
+    var lilPeanutImg = game.phaser.add.sprite(600, 350, "lilPeanutImg");
+    var bigMackImg = game.phaser.add.sprite(110, 255, "bigMackImg");
+    background.scale.setTo(0.63, 0.85);
+    optionsBtn.scale.setTo(2.8, 2.8);
+    helpBtn.scale.setTo(2.8, 2.8);
+    rankingBtn.scale.setTo(2.8, 2.8);
+    startBtn.scale.setTo(2.8, 2.8);
+    titleInline.scale.setTo(0.6, 0.6);
+    lilPeanutImg.scale.setTo(6, 6);
+    bigMackImg.scale.setTo(6, 6);
+
+    startBtn.inputEnabled = true;
+    startBtn.events.onInputDown.add(function(){
+      game.phaser.state.start("Level1");
+  });
   }
 };
